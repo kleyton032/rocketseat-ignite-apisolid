f2197cb2783df094b3e70b096d1a8eedecf8182c
Kleyton João <kleyton.joao@gmail.com>
2023-04-17 09:35:43 -0300

    error customization

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -5,6 +5,7 @@ import { hash } from 'bcryptjs'
 import { z } from 'zod'
 import { RegisterUseCase } from '@/uses-cases/register'
 import { PrismaUserRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '@/uses-cases/errors/user-already-exists-error'
 
 export async function register(req: FastifyRequest, res: FastifyReply) {
     const registerBodySchema = z.object({
@@ -18,7 +19,7 @@ export async function register(req: FastifyRequest, res: FastifyReply) {
     try {
         const userRepository = new PrismaUserRepository()
         const registerUseCase = new RegisterUseCase(userRepository)
-        
+
         await registerUseCase.execute({
             name,
             email,
@@ -26,7 +27,11 @@ export async function register(req: FastifyRequest, res: FastifyReply) {
         })
 
     } catch (error) {
-        return res.status(409).send()
+        if (error instanceof UserAlreadyExistsError) {
+            return res.status(409).send({ message: error.message })
+        }
+
+        throw error
     }
 
 
