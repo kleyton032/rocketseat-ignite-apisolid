5cebfdda75c8bb838c220417bab07787feeb980c
Kleyton João <kleyton.joao@gmail.com>
2023-05-04 15:25:05 -0300

    created fetch nearby gyms use case

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,11 +1,10 @@
 import { User, Prisma, Gym } from "@prisma/client";
 import { UsersRepository } from "../users-repository";
 import { randomUUID } from "node:crypto";
-import { GymsRepository } from "../gyms-repository";
+import { FindManyNearbyParams, GymsRepository } from "../gyms-repository";
+import { getDistanceBetweenCoordinates } from "@/ultis/get-distance-between-coordinates";
 
 export class InMemoryGymsRepository implements GymsRepository {
-   
-
 
     public items: Gym[] = []
 
@@ -19,18 +18,33 @@ export class InMemoryGymsRepository implements GymsRepository {
         return gym
     }
 
-    async searchMany(query: string, page: number){
+    async searchMany(query: string, page: number) {
         return this.items
-        .filter((item) => item.title.includes(query))
-        .slice((page-1) * 20, page * 20)
+            .filter((item) => item.title.includes(query))
+            .slice((page - 1) * 20, page * 20)
     }
 
-   async create(data: Prisma.GymCreateInput) {
+    async findManyNearby(params: FindManyNearbyParams) {
+            return this.items.filter((item) => {
+                const distance = getDistanceBetweenCoordinates(
+                    { latitude: params.latitude, longitude: params.longitude },
+                    { latitude: item.latitude.toNumber(), longitude: item.longitude.toNumber() }
+                )
+
+                return distance < 10
+            })
+
         
+
+    }
+
+
+    async create(data: Prisma.GymCreateInput) {
+
         const gym = {
             id: data.id ?? randomUUID(),
             title: data.title,
-            description:data.description ?? null,
+            description: data.description ?? null,
             phone: data.phone ?? null,
             latitude: new Prisma.Decimal(data.latitude.toString()),
             longitude: new Prisma.Decimal(data.longitude.toString()),
